9537230ef3951b9ff8233723abbaec784db29e02
yuvar <yuvar@YUVARAJAN>
2022-06-12 18:13:33 +0530

    Add project files.

---
+++ Rusada/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rusada.Models;
+using System.Diagnostics;
+
+namespace Rusada.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAir<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new AirCraftViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Rusada/Models/AirCraftViewModel.cs
@@ -0,0 +1,12 @@
+namespace Rusada.Models
+{
+    public class AirCraftViewModel
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Registration { get; set; }
+        public DateTime Time { get; set; }
+
+    }
+}
